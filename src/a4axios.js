3df5b4416666ecdbb288aafcdb903f92e13ee8fc
javkhaa8674 <javkhaa8674@gmail.com>
2025-04-22 12:10:58 +0800

    ok

--- src/a4axios.js
+++ src/a4axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://a4api-zhbjnbok5q-uc.a.run.app/",
-  // baseURL: "http://127.0.0.1:5004/a4youandme-store/us-central1/a4api/",
+ //  baseURL: "http://127.0.0.1:5004/a4youandme-store/us-central1/a4api/",
 });
 export default instance;
